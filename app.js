a5fac51d5e263275c351d3b8402cd9ec6ffecece
shidesheng <1037681793@qq.com>
2018-09-22 10:14:13 +0800

    生成excel

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fs = require("fs")
+var url = require('url');
 
 var indexRouter = require('./routes/index');
 var indexContent = require('./routes/indexContent');
 var usersRouter = require('./routes/users');
+
+var router = express.Router();
+//获取post Body中的数据
+const bodyParser=require('body-parser');
+
+//生成excel需要
+var nodeExcel = require('excel-export');
+const disableLayout ={layout: false};
+
+
+
 
 //form表单需要的中间件。
 var mutipart= require('connect-multiparty');
@@ -21,44 +33,49 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(bodyParser.urlencoded({
+   extended: false,                 //扩展模式
+   limit:    2*1024*1024           //限制-2M
+  }));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//session 需要
+var session = require('express-session');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/index', indexContent);
 app.use('/users', usersRouter);
+app.use('/api', router);
 
-
+router.use(session({
+  excelData:"",
+  secret: '1234567890QWERTY'
+}))
 //这里就是接受form表单请求的接口路径，请求方式为post。
 app.post('/upload',mutipartMiddeware,function (req,res) {
   const { files } = req;
  
   var dataList = [];
+ 
   if(files.myfile instanceof Array){
     for(var k = 0; k <files.myfile.length ; k++){
-      dataList[k] = readFile(files.myfile[k]);
+      var data = {};
+      data.list = readFile(files.myfile[k]);
+      data.name = files.myfile[k].name;
+      dataList.push(data)
     }
   }else{
-    dataList[0] =readFile(files.myfile);
+    var data = {};
+    data.list =readFile(files.myfile);
+    data.name =files.myfile.name;
+    dataList.push(data)
   }
-  console.log(dataList)
-  res.json(200,{data:dataList})
  
-  //这里打印可以看到接收到文件的信息。
- 
-  /*//do something
-  * 成功接受到浏览器传来的文件。我们可以在这里写对文件的一系列操作。例如重命名，修改文件储存路径 。等等。
-  *
-  *
-  * */
-
-  //给浏览器返回一个成功提示。
-  // res.json({
-  //   msg: '上传成功2'
-  // });
+  res.json(200,{data:dataList});
 });
 
 
@@ -71,6 +88,50 @@ function readFile(myfile){
       str= result[1];  
   return  str.replace(/\"/g,"\'")
 }
+
+router.get('/exportExcel/:id', function(req, res, next) {
+   var dataList =JSON.parse(req.session.excelData.data);
+  
+   var confs = [];
+   
+  for (var i = 0; i < dataList.length; i++) {
+   
+     var conf = {};
+     conf.cols = [];
+     conf.rows =[];
+
+     
+      for (var k = 0; k < dataList[i].text.length; k++) {
+        var rowsContent = [];
+        conf.cols = [];
+        for(var m = 0; m < dataList[i].text[k].content.length; m++){
+          rowsContent.push(dataList[i].text[k].content[m].content);
+          conf.cols.push({
+            caption: dataList[i].text[k].content[m].colspan,
+            type: 'string'
+        })
+        }
+        conf.rows.push(rowsContent)
+      }
+    
+      console.log(conf.rows)
+      conf = JSON.parse(JSON.stringify(conf));   //clone
+      conf.name = dataList[i].title;
+     
+      confs.push(conf);
+  }
+ 
+  var result = nodeExcel.execute(confs);
+  res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+  res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
+  res.end(result, 'binary');
+});
+
+//这里就是接受form表单请求的接口路径，请求方式为post。
+router.post('/saveExcelData',function (req,res) {
+  req.session.excelData = req.body;
+  res.json(200);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
